0596aa39e81d12d9f616938a000809fa5cf9eb0b
warzone2243 <lingxiaoai@gmail.com>
2021-03-31 13:20:34 -0400

    added some City methods

--- graph_stuff/city_classes.py
+++ graph_stuff/city_classes.py
@@ -4,7 +4,9 @@
 from __future__ import annotations
 from typing import Any
 from pygame_stuff.drawing import Drawable
+
 import pygame
+import math
 
 
 class _Place(Drawable):
@@ -64,12 +66,50 @@ class City:
     def __init__(self) -> None:
         self._places = dict()
 
-    def add_place(self, name: str) -> None:
+    def add_place(self, name: str, x: int, y: int) -> None:
         """Add a _Place to the dictionary with the same coordinates as the mouse click
         """
-        # TODO
+        if name not in self._places:
+            self._places[name] = _Place(name, x, y)
 
-    def add_street(self, p1: str, p2: str) -> None:
+    def add_street(self, name1: str, name2: str) -> None:
         """Connect two _Places together with a street
+
+        Raise a ValueError if either name do not appear as places in the city.
         """
-        # TODO
+        if name1 in self._places and name2 in self._places:
+            p1 = self._places[name1]
+            p2 = self._places[name2]
+
+            # Calculating distance between two points
+            x_squared = (p1.pos[0] - p2.pos[0])**2
+            y_squared = (p1.pos[1] - p2.pos[1])**2
+            dist = math.sqrt(x_squared + y_squared)
+
+            p1.neighbours.update({p2: dist})
+            p2.neighbours.update({p1: dist})
+        else:
+            raise ValueError
+            # maybe change to warning message in pygame
+
+    def get_neighbours(self, name: str) -> set:
+        """Return a set of the neighbours (the names) of the given name.
+
+        Raise a ValueError if name does not appear as a place in this city.
+        """
+        if name in self._places:
+            p = self._places[name]
+            return {neighbour.name for neighbour in p.neighbours}
+        else:
+            raise ValueError
+
+    def adjacent(self, name1: str, name2: str) -> bool:
+        """Return if name1 and name2 are adjacent places in this city.
+
+        Return False if name1 or name2 do not appear as places in this city.
+        """
+        if name1 in self._places and name2 in self._places:
+            p1 = self._places[name1]
+            return any(v2.name == name2 for v2 in p1.neighbours)
+        else:
+            return False
