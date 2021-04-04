509a0a4bbdaf261ee6bea6ec380ce92f4551b7af
warzone2243 <lingxiaoai@gmail.com>
2021-04-04 13:15:54 -0400

    testing function

--- graph_stuff/cluster_test.py
+++ graph_stuff/cluster_test.py
@@ -5,23 +5,30 @@ import pandas as pd
 
 from city_classes import City
 
-toronto = City()
 
-toronto.add_place((0, 0))
-toronto.add_place((3, 4))
-toronto.add_place((10, 15))
-toronto.add_place((9, 0))
-toronto.add_place((0, 7))
+def testing(lst: list, city: City, n_clusters: int):
+    for p in lst:
+        city.add_place(p)
+    temp = [list(x) for x in city.get_all_places()]
+    df = pd.DataFrame(temp, columns=['x', 'y'])
+
+    km = KMeans(n_clusters=n_clusters, init='k-means++')
+    km.fit_predict(df)
+    centers = km.cluster_centers_
+
+    df.plot(x='x', y='y', kind='scatter', c='blue', s=50)
+    plt.scatter(centers[:, 0], centers[:, 1], c='red', s=15)
+
+    plt.show()
 
 
-temp = [list(x) for x in toronto.get_all_places()]
-df = pd.DataFrame(temp, columns=['x', 'y'])
+if __name__ == '__main__':
+    coords = [(19, 15), (16, 12), (11, 12), (1, 13), (7, 13), (4, 3), (6, 5), (11, 9), (3, 13),
+              (9, 10), (2, 11), (8, 1), (18, 13), (16, 17), (3, 13), (7, 2), (17, 8), (18, 12),
+              (14, 13), (14, 10)]
+    toronto = City()
+    num_clusters = 5
+    testing(coords, toronto, num_clusters)
 
-km = KMeans(n_clusters=2, init='k-means++')
-km.fit_predict(df)
-centers = km.cluster_centers_
 
-df.plot(x='x', y='y', kind='scatter', c='blue', s=50)
-plt.scatter(centers[:, 0], centers[:, 1], c='red', s=15)
 
-plt.show()
